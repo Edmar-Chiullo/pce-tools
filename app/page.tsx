7a38ed2747ae607a29f4cc214945e719ee5757a0
Edmar-Chiullo <ecschiullo@gmail.com>
2025-09-13 12:13:22 -0300

    pce-tools new version BETA (correção arvore...)

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
         <div className="flex flex-col justify-between text-center w-70 h-[97vh] px-6 py-4 bg-zinc-100 rounded-2xl border-2 border-zinc-300">
             <h1 className="text-4xl font-bold p-2 rounded-[4px] text-zinc-500">PCE Tools</h1>
             <Link href="/pages/login" className="w-full">
-                <button className="h-10 w-full bg-zinc-950 text-white rounded hover:bg-zinc-900 hover:cursor-pointer">Login</button>
+                <button className="h-10 w-full bg-zinc-950 text-white rounded hover:bg-zinc-900 hover:cursor-pointer">Entrar</button>
             </Link>
         </div>
         <div className="flex flex-col justify-center gap-8 w-full h-[97vh] text-center bg-zinc-300 rounded-2xl border-2 border-zinc-400">
